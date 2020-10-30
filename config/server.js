c95490c019c19c536cc801a429f4066d4e39d09e
espring <espring@gmail.com>
2020-10-30 18:26:36 +0800

    feat: first version of back-end project template

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee9b50fb622879a4c210abdbf3608b40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f3abc411-5a14-4cec-850c-d1d5c35ba196'
+};
